7d5adab6e4a40efd579be91801ef70304790073d
Ryan Shane <ryanscsh@icloud.com>
2022-05-02 15:33:35 -0400

    final responsiveness fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
-import { Navbar, Pivot, Sidebar, TitleCard } from './components';
+import { Navbar, Sidebar, TitleCard } from './components';
 import { AboutMe, Footer, News } from './containers';
 
 import './App.css';
 
 const App = () => (
     <div className="App">
-      <div className='pivot'></div>
+      <div className='pivot'>
+        <TitleCard />
+      </div>
       <div className='gradient__bg'>
         <div className='page_wrapper'>
           <Navbar />
-          <TitleCard />
           <Sidebar />
         </div>
         <AboutMe />

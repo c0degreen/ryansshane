753f2a19853ece75bf15a9ce44a94dea87cf1604
Ryan Shane <ryan@ryansshane.com>
2022-06-09 19:35:47 -0600

    remove yt link

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -26,7 +26,6 @@ const Footer = () => {
         <div className='rss__footer-footer_links'>
           <a href='https://instagram.com/ryansshane'><p>Instagram</p></a>
           <a href='https://www.strava.com/athletes/44531359'><p>Strava</p></a>
-          <a href='https://youtube.com/'><p>YouTube</p></a>
           <a href='mailto:ryan@ryansshane.com'><p>Email</p></a>
         </div>
         <div className='rss__footer-footer_copyright'><p>&copy; Copyright {new Date().getFullYear()} Ryan Shane LLC</p></div>

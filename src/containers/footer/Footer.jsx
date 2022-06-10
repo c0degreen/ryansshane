14dfe974c9130f1a857f236bdf3eaa7a817f3550
Ryan Shane <ryan@ryansshane.com>
2022-06-09 19:25:33 -0600

    added kaulig and filip as partners

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -6,7 +6,7 @@ import './partners.css';
 function Partner(props) {
   return (
     <div className='partner'>
-      <a href={props.href}><div className={`partner-logo ${props.name}`}></div></a>
+      <a href={props.href}><div role="img" alt={props.alt} className={`partner-logo ${props.name}`}></div></a>
     </div>
   )
 }
@@ -14,14 +14,22 @@ function Partner(props) {
 const Footer = () => {
   return (
     <div className='rss__footer'>
-     
+      <div className='rss__footer-partners black' id='partners'>
+        <h1 data-title='Partners'>Partners</h1>
+        <div className='rss__footer-partners_logos'>
+          <Partner name='filipandco' alt='Filip and Co. Fine Watches' href='https://filipandcowatches.com' />
+          <Partner name='kauligracing' alt='Kaulig Racing' href='https://kauligracing.com' />
+        </div>
+        <p>Want to become a partner? Click <a href='mailto:partner@ryansshane.com'>here</a>.</p>
+      </div>
       <div className='rss__footer-footer'>
         <div className='rss__footer-footer_links'>
           <a href='https://instagram.com/ryansshane'><p>Instagram</p></a>
           <a href='https://www.strava.com/athletes/44531359'><p>Strava</p></a>
+          <a href='https://youtube.com/'><p>YouTube</p></a>
           <a href='mailto:ryan@ryansshane.com'><p>Email</p></a>
         </div>
-        <div className='rss__footer-footer_copyright'><p>&copy; Copyright {new Date().getFullYear()} Ryan Shane</p></div>
+        <div className='rss__footer-footer_copyright'><p>&copy; Copyright {new Date().getFullYear()} Ryan Shane LLC</p></div>
       </div>
     </div>
   )

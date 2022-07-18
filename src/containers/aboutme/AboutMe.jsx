701f0ec0c77307a9d70b5dfaa0a728af40bbe036
Ryan Shane <ryan@ryansshane.com>
2022-07-18 17:30:52 -0600

    please work

--- src/containers/aboutme/AboutMe.jsx
+++ src/containers/aboutme/AboutMe.jsx
@@ -46,6 +46,7 @@ const AboutMe = () => {
           <h2>Olympic Trials at 17y</h2>
           <h3>One of 12 to earn an automatic Olympic Trials spot.</h3>
           <h3>2x Olympic Trials Finalist.</h3>
+          <h3>41.151 second 500m.</h3>
         </>
       } src={about_two} />
     </div>

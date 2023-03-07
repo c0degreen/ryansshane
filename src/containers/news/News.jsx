ddb1b6baf3ade3befd8f995542a47c2729f32cc1
Ryan Shane <ryanscsh@icloud.com>
2023-03-06 22:07:05 -0700

    updated news

--- src/containers/news/News.jsx
+++ src/containers/news/News.jsx
@@ -11,15 +11,23 @@ const News = () => {
       <h2 className='h1'><b className='serif' data-title='Ryan Shane'>Ryan Shane</b> in the news</h2>
       <div className='rss__news-container'>
         <div className='groupA'>
-          <Article href='https://www.cleveland.com/community/2022/06/shaker-heights-speedskater-ryan-shane-becomes-newest-member-of-us-national-team.html'
-                   src='https://www.cleveland.com/resizer/TRI1uw5jM_maIBosswT30zXIS4o=/800x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/3L4SG2BQVZEDZOLZTR32EVAQJU.jpeg'
-                   organization='Cleveland.com'
-                   date='June 8, 2022'
-                   title='Shaker Heights speedskater Ryan Shane becomes newest member of U.S. National Team'
-                   content='Speedskater and recent Shaker Heights High School graduate Ryan Shane has been named to the 2022-23 U.S. Short Track National Team. Shane joins the team following a breakout season that saw him compete in the U.S. Olympic Trials last December outside of Salt Lake City'
+        <Article href='https://www.teamusa.org/US-Speedskating/News/Featured-News/2023/January/30/Marcus-Howard-Captures-Pair-Of-Medals-At-Short-Track-Junior-Worlds'
+                   src='https://www.teamusa.org/-/media/US_Speedskating/News-Story-Photos/2022/Short-Track/ST-jr-WCH.jpg?h=756&la=en&w=828&hash=1BA2BF5F747AF5BCAEA6680472C0300E2F316FFC'
+                   organization='TeamUSA.com'
+                   date='January 30, 2023'
+                   title='USA captures pair of medals at Short Track Junior Worlds'
+                   content='Marcus Howard teamed up with Seung-Min Kwon, Justin Liu and Ryan Shane for a third-place finish in the 3000m Relay. They finished with a time of 4:01.863, trailing winner South Korea and Japan.'
           />
+         
         </div>
         <div className='groupB'>
+         <Article href='https://www.cleveland.com/community/2022/06/shaker-heights-speedskater-ryan-shane-becomes-newest-member-of-us-national-team.html'
+                    src='https://www.cleveland.com/resizer/TRI1uw5jM_maIBosswT30zXIS4o=/800x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/3L4SG2BQVZEDZOLZTR32EVAQJU.jpeg'
+                    organization='Cleveland.com'
+                    date='June 8, 2022'
+                    title='Shaker Heights speedskater Ryan Shane becomes newest member of U.S. National Team'
+                    content='Speedskater and recent Shaker Heights High School graduate Ryan Shane has been named to the 2022-23 U.S. Short Track National Team. Shane joins the team following a breakout season that saw him compete in the U.S. Olympic Trials last December outside of Salt Lake City'
+          />
           <Article  href='https://www.youtube.com/watch?v=9_HNyKEkiGY'
                     src='https://i.ytimg.com/vi/9_HNyKEkiGY/hq720.jpg' 
                     organization='WKYC Olympic Zone'
@@ -32,12 +40,6 @@ const News = () => {
                     date='November 22, 2021'
                     title='Shaker Heights speed skater Ryan Shane on short track to Winter Olympic bid' 
                     content='With the 2022 Winter Olympics on the horizon, local short track speed skater Ryan Shane now finds himself one step away from a trip to Beijing.' />
-          <Article  href='https://www.clevelandjewishnews.com/news/sports/shaker-heights-senior-qualifies-for-us-olympic-trials/article_4aa382a8-2d17-11ec-80b6-13d68471c338.html'
-                    src='https://bloximages.chicago2.vip.townnews.com/clevelandjewishnews.com/content/tncms/assets/v3/editorial/9/4a/94a089b4-2d17-11ec-8686-fb2b26bd3ed1/61686e8041ed0.image.jpg' 
-                    organization='Cleveland Jewish News'
-                    date='October 14, 2021'
-                    title='Shaker Heights senior qualifies for US Olympic Trials' 
-                    content='The road to the Olympics is daunting. Athletes of any sports need to constantly push their bodies to the limit, sacrifice sleep and social lives, and stay mentally healthy while pursuing their dream.' />
           <Article  href='https://www.wkyc.com/video/sports/olympics/shaker-heights-speed-skater-ryan-shane-to-compete-at-us-olympic-trials/95-6af4a55b-80e9-41f4-b24d-6881f6d1ed91'
                     src={alt_photo} 
                     organization='WKYC Channel 3'

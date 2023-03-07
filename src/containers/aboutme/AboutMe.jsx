bf4546a14d52c1355e0afae5e52b6deab7f4de7a
Ryan Shane <ryanscsh@icloud.com>
2023-03-06 21:35:44 -0700

    updated about me

--- src/containers/aboutme/AboutMe.jsx
+++ src/containers/aboutme/AboutMe.jsx
@@ -35,18 +35,18 @@ const AboutMe = () => {
       <AboutMeElement number='one' text={
         <>
           <h2>Upcoming Talent</h2>
-          <h3>Breakout season in 2021-22.</h3>
-          <h3>2x US Junior Championships Bronze.</h3>
-          <h3>8x A Finals in 3 races in 2021-22.</h3>
+          <h3>2x Olympic Trials Finalist.</h3>
+          <h3>2022-23 World Cup Team.</h3>
+          <h3>Joined US National Team in 2022.</h3>
           <h3>41.151 500m.</h3>
         </>
       } src={about_one} />
       <AboutMeElement number='two' text={
         <>
-          <h2>Olympic Trials at 17y</h2>
-          <h3>One of 12 to earn an automatic Olympic Trials spot.</h3>
-          <h3>2x Olympic Trials Finalist.</h3>
-          <h3>41.151 second 500m.</h3>
+          <h2>Junior Star</h2>
+          <h3>2023 World Junior Championship Bronze Medalist.</h3>
+          <h3>5x US Junior Medalist.</h3>
+          <h3>2023 World Junior and Junior National Team.</h3>
         </>
       } src={about_two} />
     </div>

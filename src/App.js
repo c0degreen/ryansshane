5c965ab79acb2afcd166c99e5ac285c77655eec6
Ryan Shane <ryanscsh@icloud.com>
2024-04-27 17:14:32 -0600

    rebuilt pt1

--- src/App.js
+++ src/App.js
@@ -11,12 +11,7 @@ const App = () => (
         <TitleCard />
       </div>
       <div className='gradient__bg'>
-        <div className='page_wrapper'>
-          <Navbar />
-        </div>
-        <AboutMe />
-        <News />
-        <Footer />
+       
       </div>
     </div>
   );
